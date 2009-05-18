5d987ec7e820f5ed45d3f805ca16bd187c755d98
Jason Zaugg <jason@jason-zauggs-computer.local>
2009-05-18 23:07:42 +0200

    Document call by name.

--- lessons/src/main/scala/retronym/lessons/Trail.scala
+++ lessons/src/main/scala/retronym/lessons/Trail.scala
@@ -1,12 +1,13 @@
 package retronym.lessons
 
 import _root_.org.specs.runner.ScalaTestSuite
+import callbyname.CallByName
 import erasure.Erasure
 import control.Control
 import option.OptionalValues
 import org.spex.Specification
 
 object Trail extends Specification {
- "Learning Trail" isSpecifiedBy(testing.specs.Sample, Control, Erasure, OptionalValues)
+ "Learning Trail" isSpecifiedBy(testing.specs.Sample, Control, CallByName, Erasure, OptionalValues)
 }
 
--- lessons/src/main/scala/retronym/lessons/callbyname/CallByName.scala
+++ lessons/src/main/scala/retronym/lessons/callbyname/CallByName.scala
@@ -2,7 +2,24 @@ package retronym.lessons.callbyname
 
 import _root_.org.spex.Specification
 
-
+/**
+ * 4.6.1 The type of a value parameter may be preÞxed by =>, e.g. x : => T . The type of
+ * such a parameter is then the parameterless method type => T . This indicates that
+ * the corresponding argument is not evaluated at the point of function application,
+ * but instead is evaluated at each use within the function. That is, the argument is
+ * evaluated using call-by-name.<br/>
+ * <hr/>
+ * Practical when: <br/>
+ * <ul>
+ *   <li>in a logging API, so you don't create the message unless the threshold is set.</li>
+ *   <li>to implement a new control structure, such as a do-while loop</li>
+ *   <li>DSL creation</li>
+ * </ul>
+ * Drawbacks:<br/>
+ * <ul>
+ *   <li>When reading client code, a programmer expects method arguments to be evaluated eagerly. Better tool support may mitigate this.</li>
+ * </ul>
+ */
 object CallByName extends Specification {
   "CallByName" should {
     trait StringSource {
